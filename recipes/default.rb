31a3290a8678b5ed0b5ee938752e703c9a573081
Stephen Lauck <stpehen.lauck@gmail.com>
2012-10-10 17:03:41 -0700

    add ubuntu supoort for resolvconf

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,23 @@
-template "/etc/resolv.conf" do
-  source "resolv_conf.erb"
-  mode 0644
-  variables(
-    :domain => node.default.domain,
-    :nameservers => node[:resolv][:nameservers].empty? ? '' : node[:resolv][:nameservers]
-  )
+case node['platform']
+when "debian", "ubuntu"
+  template "/etc/resolvconf/resolv.conf.d/base" do
+    source "resolvconf_base.erb"
+    mode 0644
+    variables(
+      :nameservers => node[:resolv][:nameservers].empty? ? '' : node[:resolv][:nameservers]
+    )
+  end
+when "smartos"
+  template "/etc/resolv.conf" do
+    source "resolv_conf.erb"
+    mode 0644
+    variables(
+      :domain => node.default.domain,
+      :nameservers => node[:resolv][:nameservers].empty? ? '' : node[:resolv][:nameservers]
+    )
+  end
 end
+
+
+
+
